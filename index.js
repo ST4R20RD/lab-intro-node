2ff5e29f0feb090154e8c10f211ceb34c82754ee
Gonçalo Estrelado <goncalo.estrelado@hotmail.com>
2022-01-25 21:13:46 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=>a-b)
+    this.length=this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos<=this.length){
+      return this.items[pos]
+    }else{
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else {    
+    return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else {    
+    return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0){
+      return 0
+    } else {
+    return this.items.reduce((prevNumber, currNumber) => prevNumber + currNumber)
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length <= 0){s
+      return 0;
+    } else {
+      return this.sum()/this.items.length
+    } 
+  }
 }
 
 module.exports = SortedList;
